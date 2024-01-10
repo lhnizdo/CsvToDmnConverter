33bba10ead427105ffff87c61e498d0d7e53be3c
qkhnizdolub <lubos.hnizdo@cez.cz>
2024-01-10 14:08:20 +0100

    Enforced UTF-8 encoding for DMN output

--- src/main/java/at/jit/service/DmnFileExporter.java
+++ src/main/java/at/jit/service/DmnFileExporter.java
@@ -6,12 +6,14 @@ import org.camunda.bpm.model.dmn.DmnModelInstance;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 public class DmnFileExporter {
     public void exportToDmnFile(DmnModelInstance dmnModelInstance, String path) {
         try (FileWriter fileWriter = new FileWriter(path)) {
             PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.print(Dmn.convertToString(dmnModelInstance));
+            String dmnToWrite = new String(Dmn.convertToString(dmnModelInstance).getBytes(), StandardCharsets.UTF_8);
+            printWriter.print(dmnToWrite);
         } catch (IOException e) {
             e.printStackTrace();
         }
